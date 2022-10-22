9a227f4ff20977d1fe5b82ee25af739ad7b061e5
Witold Swiatowy <swiatowywitold@gmail.com>
2022-10-22 16:07:24 +0200

    test(CRRS): add first test to mapper

---
+++ src/test/java/pl/mojeprojecty/conferenceroomreservationsystem/organization/OrganizationMapperTest.java
@@ -0,0 +1,36 @@
+package pl.mojeprojecty.conferenceroomreservationsystem.organization;
+
+import org.junit.jupiter.api.Test;
+import pl.mojeprojecty.conferenceroomreservationsystem.organization.model.OrganizationDto;
+import pl.mojeprojecty.conferenceroomreservationsystem.organization.model.OrganizationEntity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static java.util.UUID.randomUUID;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrganizationMapperTest {
+
+    public static final LocalDateTime NOW = LocalDateTime.now();
+
+    @Test
+    void shouldReturnCorrectDtoObject() {
+        //given
+        OrganizationEntity organization = new OrganizationEntity();
+        organization.setName(randomUUID().toString());
+        organization.setId(1L);
+        organization.setVersion(1L);
+        organization.setCreateDate(NOW);
+        organization.setUpdateDate(NOW);
+
+        //when
+        OrganizationDto organizationDto = OrganizationMapper.toOrganizationDto(organization);
+
+        //then
+        assertThat(organizationDto.getName()).isEqualTo(organization.getName());
+        assertThat(organizationDto.getId()).isEqualTo(organization.getId());
+    }
+
+}
